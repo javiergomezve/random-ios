d97e83a05feda161065c672ca14e7b5a90225d9b
Javier Gomez <javiergomezve@gmail.com>
2018-12-10 10:53:43 -0400

    Initial Commit

---
+++ Random/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Random
+//
+//  Created by Javier Gomez on 12/10/18.
+//  Copyright © 2018 Javier Gomez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
