989648c603c90e2aac89af372a6aa1af542cd4e7
Javier Gomez <javiergomezve@gmail.com>
2018-12-12 18:45:53 -0400

    Add comment to database

---
+++ Random/Model/Comment.swift
@@ -0,0 +1,44 @@
+//
+//  Comment.swift
+//  Random
+//
+//  Created by Javier Gomez on 12/12/18.
+//  Copyright © 2018 Javier Gomez. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class Comment {
+    private(set) var username: String!
+    private(set) var timestamp: Date!
+    private(set) var commentTxt: String!
+    private(set) var documentId: String!
+    
+    init(username: String, timestamp: Date, commentTxt: String, documentId: String) {
+        self.username = username
+        self.timestamp = timestamp
+        self.commentTxt = commentTxt
+        self.documentId = documentId
+    }
+    
+    class func parseData(snapshot: QuerySnapshot?) -> [Comment] {
+        var comments = [Comment]()
+        
+        guard let snap = snapshot else { return comments }
+        
+        for document in snap.documents {
+            let data = document.data()
+            let username = data[USERNAME] as? String ?? "Anonymous"
+            let timestamp = data[TIMESTAMP] as? Date ?? Date()
+            let commentTxt = data["commentTxt"] as? String ?? "commentTxt"
+            let documentId = document.documentID
+            
+            let newComment = Comment(username: username, timestamp: timestamp, commentTxt: commentTxt, documentId: documentId)
+            
+            comments.append(newComment)
+        }
+        
+        return comments
+    }
+}
--- Random/View/CommentCell.swift
+++ Random/View/CommentCell.swift
@@ -17,4 +17,14 @@ class CommentCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
     }
+    
+    func configureCell(comment: Comment) {
+        usernameLbl.text = comment.username
+        commentLbl.text = comment.commentTxt
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM d, hh:mm"
+        let timestamp = formatter.string(from: comment.timestamp)
+        timestampLbl.text = timestamp
+    }
 }
